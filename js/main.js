36c4148af0b03fe9b4b28bae7119fa64de9a08ff
saidmoussadek <saiddus24@gmail.com>
2022-07-05 18:43:55 +0200

    push

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+let generaBiglietto= document.getElementById('bottone');
+
+generaBiglietto.addEventListener('click', function(){
+  let km = document.getElementById('distanza').value;
+  let eta= document.getElementById('fasciaEta').Value;
+  console.log(km)
+  console.log(fasciaEta)
+
+
+     
+
+
+
+
+
+});
